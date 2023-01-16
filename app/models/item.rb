0d11b9fa8cd5fd5887537c5de6e9afb2f3e363e2
dwarfsawfish <akamaki_0729@yahoo.co.jp>
2023-01-16 16:49:17 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   validates :name, :content, :image, presence: true
 
   validates :price,             presence: true,
-                                numericality: { with: /\A[0-9]+\z/, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+                                numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   validates :category_id,       numericality: { other_than: 1, message: 'カテゴリーを選択してください' }
   validates :status_id,         numericality: { other_than: 1, message: '商品の状態を選択してください' }
