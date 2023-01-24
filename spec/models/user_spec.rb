9a458081a284dd9cc15c8d899e92c7ab12117d34
dwarfsawfish <akamaki_0729@yahoo.co.jp>
2023-01-24 21:54:47 +0900

    database.yml修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe User, type: :model do
       end
     end
 
-    context '新規登録できない場合' do
+    context '新規登録ができない場合' do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
         @user.valid?
