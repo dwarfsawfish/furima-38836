5ea8d7dd6954e0c0aa951c0c5c507b14c2dd8cf8
dwarfsawfish <akamaki_0729@yahoo.co.jp>
2023-01-25 18:03:37 +0900

    あ

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,6 @@
+#「デプロイするのはmainブランチです」という設定を追加する意味
+set :branch, 'main'
+
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.17.1'
 
@@ -24,8 +27,6 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-#「デプロイするのはmainブランチです」という設定を追加する意味
-set :branch, 'main'
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
